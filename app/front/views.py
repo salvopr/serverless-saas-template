204add5b6c49426e562ca2e0eb38de542a6a0418
smirnov-am <msc.smirnov.am@gmail.com>
2021-10-29 15:56:50 +0200

    add robots block to avoid 404s

--- app/front/views.py
+++ app/front/views.py
@@ -1,8 +1,13 @@
 from . import front_blueprint
 
-from flask import render_template
+from flask import render_template, send_from_directory, current_app
 
 
 @front_blueprint.route('/', methods=["GET"])
 def index():
     return render_template('landing.html')
+
+
+@front_blueprint.route("/robots.txt", methods=["GET"])
+def robots():
+    return send_from_directory(current_app.static_folder, 'robots.txt')
